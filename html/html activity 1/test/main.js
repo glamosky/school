a5d5ceb5ab0f4cd1f56fd6cbbf3d1e8f97c713dd
glamosky <frankcayaco@gmail.com>
2025-07-16 20:54:35 +0800

    new activities

---
+++ html/html activity 1/test/main.js
@@ -0,0 +1,9 @@
+const celsius= C;
+
+console.log(293 - 273);
+
+//Celsius to Fahrenheit
+const Fahrenheit(20 * 9 / 5 + 32);
+
+console.log(Fahrenheit);
+console.log('The temperature is' ${Fahrenheit}'degrees fahrenheit');
---
+++ javascript/activity 2/main.js
@@ -0,0 +1,16 @@
+const name = "Frank Cayaco";
+const my_age = 22;
+const dog_age = (my_age - 2) * 4 + (10.5 * 2);
+const georgia = 7;
+const georgia_human = 21 + (georgia - 2) * 4;
+const ascii = `
+            __
+(\,--------'()'--o
+ (_    ___    /~"
+  (_)_)  (_)_)
+
+`;
+
+console.log(`My name is ${name}. I am ${my_age} years old in human years which is ${dog_age} years old in dog years. \n`);
+console.log(`This is Georgia, my dog! She is ${georgia} years old in dog years, which makes her ${georgia_human} years old in human years.`);
+console.log(ascii);
