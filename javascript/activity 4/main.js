17e83b9bdad0b608b23d9b9e2047234860930f32
glamosky <frankcayaco@gmail.com>
2025-07-20 00:29:25 +0800

    added alternative

--- javascript/activity 4/main.js
+++ javascript/activity 4/main.js
@@ -1,7 +1,7 @@
 // Activity 4
 
 // change values of age and isEarlyRegistered for testing
-const age = 78;             // could be any number
+const age = 19;             // could be any number
 const isEarlyRegistered = true;   // true or false
 let raceNumber = Math.floor(Math.random() * 1000);
 const youthRaceNumber = Math.abs(raceNumber - 1000);
@@ -18,3 +18,24 @@ else if (age < 18) {
 else if (age === 18) {
     console.log("Please see the registration desk.");
 }
+
+//alternate
+// const adult = age > 18;
+// const youth = age <= 18;
+
+// if (adult) {
+//     if (isEarlyRegistered) {
+//         console.log(`Runner ${raceNumber += 1000}, your race will start at 9:30 AM.`);
+//     }
+//     else if (!isEarlyRegistered) {
+//         console.log(`Runner ${raceNumber += 1000}, your race will start at 11:00 AM. Late adults run at 11:00 AM.`);
+//     }
+// }
+// else if (youth) {
+//     if (age < 18) {
+//         console.log(`Runner ${youthRaceNumber}, your race will start at 12:30 PM. Youth runners start at 12:30 PM (regardless of registration).`);
+//     }
+//     else if (age === 18) {
+//         console.log("Please see the registration desk.");
+//     }
+// }
