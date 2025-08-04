43b42a7244214b61d060da3e33934bb856309ce3
glamosky <frankcayaco@gmail.com>
2025-08-04 20:34:13 +0800

    two sctivities from csharp and js

---
+++ CSharp/CSharpProjects/LeapYear/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter year here: ");
+            string inputline = Console.ReadLine();
+            int myVar = int.Parse(inputline);
+            Console.WriteLine("User has entered year " + inputline);
+
+            if (myVar % 4 == 0 && myVar % 100 == 0 && myVar % 400 == 0)
+            {
+                Console.WriteLine("The year you entered IS A LEAP YEAR");
+            }
+
+            else if (myVar % 4 == 0 && myVar % 100 == 0)
+            {
+                Console.WriteLine("NOT A LEAP YEAR");
+            }
+            else if (myVar % 4 == 0)
+            {
+                Console.WriteLine("The year you entered IS A LEAP YEAR");
+            }
+
+            else
+            {
+                Console.WriteLine("NOT A LEAP YEAR");
+            }
+        }
+    }
+}
--- javascript/activity 6/trainingDays.js
+++ javascript/activity 6/trainingDays.js
@@ -1,46 +1,51 @@
 // The scope of `random` is too loose 
-const random = Math.floor(Math.random() * 3);
-
 const getRandEvent = () => {
-    if (random === 0) {
-      return 'Marathon';
-    } else if (random === 1) {
-      return 'Triathlon';
-    } else if (random === 2) {
-      return 'Pentathlon';
-    }
-  };
-  
-  // The scope of `days` is too tight 
-  const getTrainingDays = event => {
-    
-    if (event === 'Marathon') {
-      let days = 50;
-    } else if (event === 'Triathlon') {
-      let days = 100;
-    } else if (event === 'Pentathlon') {
-      let days = 200;
-    }
-  
-    return days;
-  };
-  
-  // The scope of `name` is too tight 
-  const logEvent = event => {
-    const name = 'Nala';
-    console.log(`${name}'s event is: ${event}`);
-  };
-  
-  const logTime =  days => {
-    const name = 'Nala';
-    console.log(`${name}'s time to train is: ${days} days`);
-  };
-  
-  const event = getRandEvent();
-  const days = getTrainingDays(event);
-  // Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+  const random = Math.floor(Math.random() * 3);
+  if (random === 0) {
+    return 'Marathon';
+  } 
+  else if (random === 1) {
+    return 'Triathlon';
+  } 
+  else if (random === 2) {
+    return 'Pentathlon';
+  }
+};
 
-  
-  logEvent(event);
-  logTime(days);
-  
+// The scope of `days` is too tight 
+const getTrainingDays = event => {
+  let days;
+  if (event === 'Marathon') {
+    days = 50;
+  } else if (event === 'Triathlon') {
+    days = 100;
+  } else if (event === 'Pentathlon') {
+    days = 200;
+  }
+  return days;
+};
+
+// The scope of `name` is too tight 
+// Move name to global scope so it can be used for different athletes
+
+const logEvent = (name, event) => {
+  console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = (name, days) => {
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+
+// First athlete
+const name = 'Nala';
+const event = getRandEvent();
+const days = getTrainingDays(event);
+logEvent(name, event);
+logTime(name, days);
+
+// Second athlete (reusable and different scope)
+const name2 = 'Warren';
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+logEvent(name2, event2);
+logTime(name2, days2);
